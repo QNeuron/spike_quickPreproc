4ab8811ded65bcf2cccd6463c14c8c3e5f06d0a2
QNeuron <quentin_g@hotmail.com>
2016-04-06 18:13:32 +0100

    Delete sliderCallback.m

--- sliderCallback.m
+++
@@ -1,8 +0,0 @@
-
-function sliderCallback(hObj,event)
-handles = guidata(hObj);
-ch = find(handles.hSlider==hObj);
-handles.thr(ch) = get(hObj,'Value');
-guidata(hObj,handles);
-replotLine(hObj,ch);
-end
